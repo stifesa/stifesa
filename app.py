6771fd114f069edc0d8a83f9ff3b7f85f0b43007
stifesa <113121744+stifesa@users.noreply.github.com>
2022-09-15 15:47:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 # Press Mayús+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+import os
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 
@@ -11,10 +11,10 @@ import pickle
 
 app = Flask(__name__)
 
-
+port = int(os.environ.get('PORT', 5000))
 @app.route("/")
 def home():
     return "hola mundo"
 
 
-app.run()
+app.run(port=port)
